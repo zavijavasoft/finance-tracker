bd58a8019c4364932cdf7268f511fb854a18bfeb
Олег Будин <okbudin@yandex.ru>
2018-08-03 19:52:42 +0300

    Новые объекты добавлены в Dagger
    Плюс даггер немного доработан

--- app/src/main/java/com/mashjulal/android/financetracker/di/DataStorageModule.kt
+++ app/src/main/java/com/mashjulal/android/financetracker/di/DataStorageModule.kt
@@ -1,10 +1,17 @@
 package com.mashjulal.android.financetracker.di
 
+import com.mashjulal.android.financetracker.App
 import com.mashjulal.android.financetracker.data.currencyconvertapi.CurrencyService
 import com.mashjulal.android.financetracker.domain.repository.*
-import com.mashjulal.android.financetracker.stub.repository.*
+import com.mashjulal.android.financetracker.domain.repository.sqliteimpl.CategoryRepositoryImpl
+import com.mashjulal.android.financetracker.domain.repository.sqliteimpl.SQLiteCore
+import com.mashjulal.android.financetracker.stub.repository.AccountRepositoryStub
+import com.mashjulal.android.financetracker.stub.repository.BalanceRepositoryStub
+import com.mashjulal.android.financetracker.stub.repository.CurrencyRepositoryStub
+import com.mashjulal.android.financetracker.stub.repository.OperationRepositoryStub
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
@@ -28,5 +35,20 @@ class DataStorageModule {
 
     @Singleton
     @Provides
-    fun providesCategoryRepository(): CategoryRepository = CategoryRepositoryStub()
+    fun providesCategoryRepository(core: SQLiteCore): CategoryRepository = CategoryRepositoryImpl(core)
+
+    @Singleton
+    @Provides
+    fun providesDatabase(app: App, @Named("database_name") databaseName: String, @Named("database_version") databaseVersion: Int): SQLiteCore {
+        return SQLiteCore(app, databaseName, databaseVersion)
+    }
+
+    @Provides
+    @Named("database_name")
+    fun provideDatabaseName() = "finance_tracker.db"
+
+    @Provides
+    @Named("database_version")
+    fun provideDatabaseVersion() = 1
+
 }
--- app/src/main/java/com/mashjulal/android/financetracker/di/DiComponents.kt
+++ app/src/main/java/com/mashjulal/android/financetracker/di/DiComponents.kt
@@ -1,13 +1,30 @@
 package com.mashjulal.android.financetracker.di
 
+import android.content.Context
+import com.mashjulal.android.financetracker.App
 import com.mashjulal.android.financetracker.presentation.editoperation.EditOperationActivity
 import com.mashjulal.android.financetracker.presentation.main.MainFragment
+import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 @Component(modules = [DataStorageModule::class, NetworkModule::class, InteractorModule::class])
 @Singleton
 interface AppComponent {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(app: App): Builder
+
+        @BindsInstance
+        fun context(context: Context): Builder
+
+        fun build(): AppComponent
+    }
+
+
+    fun inject(app: App)
     fun inject(fragment: MainFragment)
     fun inject(activity: EditOperationActivity)
 }
