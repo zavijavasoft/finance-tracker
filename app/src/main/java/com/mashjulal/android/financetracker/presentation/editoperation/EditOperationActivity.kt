09cb0cbfb525366aef3ca9033343ef2af9dc4a81
Олег Будин <okbudin@yandex.ru>
2018-08-06 04:44:18 +0300

    Moxy внедрена в опреационную активити

--- app/src/main/java/com/mashjulal/android/financetracker/presentation/editoperation/EditOperationActivity.kt
+++ app/src/main/java/com/mashjulal/android/financetracker/presentation/editoperation/EditOperationActivity.kt
@@ -5,13 +5,14 @@ import android.app.DatePickerDialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import com.davidmiguel.numberkeyboard.NumberKeyboardListener
+import com.arellomobile.mvp.MvpAppCompatActivity
+import com.arellomobile.mvp.presenter.InjectPresenter
+import com.arellomobile.mvp.presenter.ProvidePresenter
 import com.mashjulal.android.financetracker.App
 import com.mashjulal.android.financetracker.R
 import com.mashjulal.android.financetracker.domain.financialcalculations.*
@@ -27,10 +28,15 @@ private const val ARG_OPERATION_ACCOUNT = "account"
 
 private const val MAX_AMOUNT_DIGIT_COUNT = 12
 
-class EditOperationActivity : AppCompatActivity(), EditOperationPresenter.View {
+class EditOperationActivity : MvpAppCompatActivity(), EditOperationPresenter.View {
 
     @Inject
+    @InjectPresenter
     lateinit var presenter: EditOperationPresenter
+
+    @ProvidePresenter
+    fun providePresenter() = presenter
+
     private val calendar: Calendar = Calendar.getInstance()
     private lateinit var accountName: String
     private lateinit var operationType: String
@@ -38,53 +44,16 @@ class EditOperationActivity : AppCompatActivity(), EditOperationPresenter.View {
     private lateinit var accounts: List<Account>
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        App.appComponent.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_operation)
 
-        App.appComponent.inject(this)
         initLayout()
         parseArgs()
         setResult(Activity.RESULT_CANCELED)
     }
 
     private fun initLayout() {
-        numberKeyboard.setListener(object : NumberKeyboardListener {
-            override fun onNumberClicked(number: Int) {
-                val amount = etAmount.text.toString()
-
-                if (amount == "0") {
-                    if (number != 0) {
-                        etAmount.setText(number.toString())
-                    }
-                    return
-                }
-
-                val doteIndex = amount.indexOf(".")
-                val noDot = doteIndex == -1
-                val lessThanThreeDigitsAfterDot = doteIndex > amount.length - 3
-                val amountBelowLimit = amount.length < MAX_AMOUNT_DIGIT_COUNT
-
-                if ((noDot || lessThanThreeDigitsAfterDot) && amountBelowLimit) {
-                    etAmount.text.append(number.toString())
-                }
-            }
-
-            override fun onLeftAuxButtonClicked() {
-                if (!etAmount.text.contains(".")) {
-                    etAmount.text.append(".")
-                }
-            }
-
-            override fun onRightAuxButtonClicked() {
-                val amountRepr = etAmount.text.toString()
-                if (amountRepr.isNotEmpty()) {
-                    etAmount.setText(amountRepr.substring(0, amountRepr.lastIndex))
-                }
-                if (etAmount.text.isEmpty()) {
-                    etAmount.setText("0")
-                }
-            }
-        })
         btnSelectDate.setOnClickListener {
             showDateDialog()
         }
@@ -133,6 +102,7 @@ class EditOperationActivity : AppCompatActivity(), EditOperationPresenter.View {
     private fun finishEdit() {
         let {
             val amount = BigDecimal(etAmount.text.toString())
+            if (amount == BigDecimal.ZERO) return@let
             val currency = if (spinnerCurrency.selectedItem.toString() == "$") Currency.DOLLAR else Currency.RUBLE
             val date = calendar.time
             val operationTypeRepr = spinnerOperationType.selectedItem as String
@@ -196,15 +166,6 @@ class EditOperationActivity : AppCompatActivity(), EditOperationPresenter.View {
         finish()
     }
 
-    override fun onResume() {
-        super.onResume()
-        presenter.attachView(this)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        presenter.detachView()
-    }
 
     companion object {
 
